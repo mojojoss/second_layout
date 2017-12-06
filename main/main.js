de42db69ff1689db4a74b53425fe5490da56fbcd
Mojo <mojojoss1@gmail.com>
2017-12-07 00:34:00 +0700

    first_commit__add_rippleButton

---
+++ main/main.js
@@ -0,0 +1,5 @@
+import './__button/__button.css';
+import './__button/__button.js';
+import './__button/_button_color-aqua/_button_color-aqua.css';
+import './__button/_button_size-big/_button_size-big.css';
+import './__button/_button_color-red/_button_color-red.css';
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.resolve(__dirname, './main'),
+    entry: {
+        app: './main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js',
+        publicPath: '/public'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './main')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: { presets: ['es2015'] }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
