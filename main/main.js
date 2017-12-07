bcc3adad48d73a2aaccb205d90c6dd06a4cecb34
Mojo <mojojoss1@gmail.com>
2017-12-08 00:10:07 +0700

    add_arrow_buttons

--- main/main.js
+++ main/main.js
@@ -3,3 +3,7 @@ import './__button/__button.js';
 import './__button/_button_color-aqua/_button_color-aqua.css';
 import './__button/_button_size-big/_button_size-big.css';
 import './__button/_button_color-red/_button_color-red.css';
+import './__arrow-button/_arrow-button_left.css';
+import './__arrow-button/_arrow-button_right.css';
+import './__arrow-button/_arrow-button_left-disabled.css';
+import './__arrow-button/_arrow-button_right-disabled.css';
