4803557baa5694c8aa64abb8a85fadffa9039203
Mojo <mojojoss1@gmail.com>
2017-12-25 00:40:13 +0700

    Map

---
+++ js.js
@@ -0,0 +1,33 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [56.476816, 84.971598],
+            zoom: 15,
+			controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark([56.476816, 84.971598], {
+            hintContent: '',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'main/__map/_map_images/map-marker.png',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+});
